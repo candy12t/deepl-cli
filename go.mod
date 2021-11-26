34db36420c138d7c16d0ae4f484fc6fd0a9370f4
Takumi Kanada <candy12t@gmail.com>
2021-11-27 00:18:32 +0900

    Change repl test to use Client mock generated by gomock

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/candy12t/deepl-cli
 go 1.17
 
 require (
+	github.com/golang/mock v1.6.0
 	github.com/urfave/cli/v2 v2.3.0
 	gopkg.in/yaml.v2 v2.4.0
 )
--- internal/repl/repl_test.go
+++ internal/repl/repl_test.go
@@ -2,45 +2,80 @@ package repl
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"testing"
 
 	"github.com/candy12t/deepl-cli/internal/deepl"
+	"github.com/candy12t/deepl-cli/internal/deepl/mock_deepl"
+	"github.com/golang/mock/gomock"
 )
 
 func TestRepl(t *testing.T) {
-	tests := []struct {
-		name       string
-		input      *bytes.Buffer
+	type args struct {
+		inputText  string
+		sourceLang string
 		targetLang string
-		want       string
+	}
+	tests := []struct {
+		name          string
+		args          args
+		prepareMockFn func(m *mock_deepl.MockAPIClient)
+		want          string
 	}{
 		{
-			name:       "success repl",
-			input:      bytes.NewBufferString("hello"),
-			targetLang: "JA",
-			want:       fmt.Sprintf("%sこんにちわ\n%s", PROMPT, PROMPT),
+			name: "success repl",
+			args: args{
+				inputText:  "hello",
+				sourceLang: "EN",
+				targetLang: "JA",
+			},
+			prepareMockFn: func(m *mock_deepl.MockAPIClient) {
+				ctx := context.Background()
+				m.EXPECT().
+					Translate(ctx, "hello", "EN", "JA").
+					Return(&deepl.Translate{Translations: []deepl.Translation{{DetectedSourceLanguage: "EN", Text: "こんにちわ"}}}, nil)
+			},
+			want: fmt.Sprintf("%sこんにちわ\n%s", PROMPT, PROMPT),
 		},
 		{
-			name:       "failed repl because input text length is 0",
-			input:      bytes.NewBufferString("\n"),
-			targetLang: "JA",
-			want:       fmt.Sprintf("%sError: text length is 0\n%s", PROMPT, PROMPT),
+			name: "failed repl because input text length is 0",
+			args: args{
+				inputText:  "\n",
+				sourceLang: "EN",
+				targetLang: "JA",
+			},
+			prepareMockFn: func(m *mock_deepl.MockAPIClient) {},
+			want:          fmt.Sprintf("%sError: text length is 0\n%s", PROMPT, PROMPT),
 		},
 		{
-			name:       "failed repl because not specified target language",
-			input:      bytes.NewBufferString("hello"),
-			targetLang: "",
-			want:       fmt.Sprintf("%sHTTP 400: \"Value for 'target_lang' not supported.\" (https://api-free.deepl.com/v2/translate)\n", PROMPT),
+			name: "failed repl because not specified target language",
+			args: args{
+				inputText:  "hello",
+				sourceLang: "EN",
+				targetLang: "",
+			},
+			prepareMockFn: func(m *mock_deepl.MockAPIClient) {
+				ctx := context.Background()
+				m.EXPECT().
+					Translate(ctx, "hello", "EN", "").
+					Return(nil, fmt.Errorf(`HTTP 400: "Value for 'target_lang' not supported." (https://api-free.deepl.com/v2/translate)`))
+			},
+			want: fmt.Sprintf("%sHTTP 400: \"Value for 'target_lang' not supported.\" (https://api-free.deepl.com/v2/translate)\n", PROMPT),
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			out := new(bytes.Buffer)
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
 
-			client, _ := deepl.NewMockClient("https://api.deepl.com/v2", "test-auth-key")
-			Repl(client, "EN", tt.targetLang, tt.input, out)
+			mockClient := mock_deepl.NewMockAPIClient(ctrl)
+
+			tt.prepareMockFn(mockClient)
+
+			out := new(bytes.Buffer)
+			Repl(mockClient, tt.args.sourceLang, tt.args.targetLang, bytes.NewBufferString(tt.args.inputText), out)
 
 			got := out.String()
 			if got != tt.want {
